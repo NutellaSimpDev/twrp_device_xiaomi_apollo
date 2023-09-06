fbcbb7127fea468d2ec868c0c07493bf2f8050f4
NutellaDev <eduardocayovilca@gmail.com>
2023-09-05 23:03:01 -0500

    apollo: Updating fingerprint

--- twrp_apollo.mk
+++ twrp_apollo.mk
@@ -1,43 +1,29 @@
 #
-# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := apollo
-DEVICE_PATH := device/xiaomi/apollo
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-
-# Inherit from our custom product configuration
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit from umi device
+$(call inherit-product, device/xiaomi/apollo/device.mk)
+
 PRODUCT_DEVICE := apollo
 PRODUCT_NAME := twrp_apollo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 10T
-PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MANUFACTURER := xiaomi
 
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="twrp_apollo-eng 99.86.37 SP2A.220405.004 eng.noodle.20220908.072902 test-keys"
 
-
-# fastbootd stuff 
-PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery
+BUILD_FINGERPRINT := Xiaomi/twrp_apollo/umi:99.86.37/SP2A.220405.004/noodle09080728:eng/test-keys
